c441f8afaa04cff99cc2ae2010b837255c560d97
Iury Teixeira <franciury@gmail.com>
2021-11-10 15:06:49 -0300

    receiving sns/sqs messaging

--- src/main/kotlin/com/iteixeira/pocspringcloud/PocSpringCloudApplication.kt
+++ src/main/kotlin/com/iteixeira/pocspringcloud/PocSpringCloudApplication.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs
+import org.springframework.cloud.aws.messaging.config.annotation.NotificationMessage
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
@@ -62,7 +63,7 @@ class SQSConfig(
 class CustomerApiConsumer {
 
     @SqsListener(value = ["customer-register"], deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    fun listenerNewCustomer(customer: CustomerApi) {
+    fun listenerNewCustomer(@NotificationMessage customer: CustomerApi) {
         logger.info("Received SQS: {}", customer)
     }
 }
@@ -76,5 +77,3 @@ class CustomerController {
     fun hello(@PathVariable("text") t: String) = "${t}, Kotlin Customer!"
 
 }
-
-
